e97790f906f63f62ca90624369541168dcab6334
Carolina Cordero <ccalvarez@gmail.com>
2017-08-29 10:14:16 -0600

    New route '/users'. Working with 'hashtag' mode.

---
+++ src/components/User.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    User Component</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+div {
+  border-style: solid;
+  border-width: thin;
+  width: 960px;
+  height: 600px;
+  padding: 30px;
+}
+</style>
+
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,22 @@
-import Vue from 'vue'
-import Router from 'vue-router'
-import Hello from '@/components/Hello'
+import Vue from 'vue';
+import Router from 'vue-router';
+import Hello from '@/components/Hello';
+import User from '@/components/User';
 
-Vue.use(Router)
+Vue.use(Router);
 
 export default new Router({
   routes: [
     {
       path: '/',
       name: 'Hello',
-      component: Hello
-    }
-  ]
-})
+      component: Hello,
+    },
+    {
+      path: '/users',
+      name: 'Users',
+      component: User,
+    },
+  ],
+  // mode: 'history',
+});
